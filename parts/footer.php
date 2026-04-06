7a070af9d9d872e1aebc4f85e9a6bda25c5e6d84
VikIsCool123 <viktorhorvath876@gmail.com>
2026-04-06 13:44:47 +0200

    separation of the footer and the head

---
+++ parts/footer.php
@@ -0,0 +1,3 @@
+        <div class="container-footer"><!--The footer of the webpage.-->
+            <p>(this is the footer)This site was created by: Viktor Horvath, it was created as an end project for the class thats called jazyky webu. Its about a relaxation place called LuiRC as in Luises Relaxation Center.</p>
+        </div>
---
+++ parts/header.php
@@ -0,0 +1,28 @@
+        <nav class="navbar navbar-expand-lg sticky-top"><!--The navbar thats used to navigate the webpage.-->
+            <div class="container-fluid">
+                <div class="container-logo"><!--A separate conainer(div) thats created to house the logo.-->
+                    <a class="navbar-brand" href="#">
+                        <img class="logo" alt="logo for the company, a blue wawe with LuiRC written next to it" src="images/Logo.png"><!--The logo itself.-->
+                    </a>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button><!--A button to acess the hamburger menu.-->
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><!--Menu item that acts like a button. It moves/redirects you to the Home part of the webpage.-->
+                            <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                        </li>
+                            <li class="nav-item"><!--Menu item that acts like a button. It moves/redirects you to the About us part of the webpage.-->
+                            <a class="nav-link" href="about_us.html">About us</a>
+                        </li>
+                        <li class="nav-item"><!--Menu item that acts like a button. It moves/redirects you to the Veriety (i tiped it wrong in the style) part of the webpage.-->
+                            <a class="nav-link" href="variety.html">Veriety</a>
+                        </li>
+                        <li class="nav-item"><!--Menu item that acts like a button. It moves/redirects you to the Reservation part of the webpage.-->
+                            <a class="nav-link" href="reservation.html">Reservation</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
