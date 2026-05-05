12382ff2ca9435dbd0729aa985288f0839b7dd27
VikIsCool123 <viktorhorvath876@gmail.com>
2026-05-05 16:35:37 +0200

    made some changes

--- create.php
+++ create.php
@@ -13,11 +13,26 @@ if (isset($_POST["res-button"])) {
     $password = $_POST["InputPassword"];
     $password = md5($password);
 
-    $stmt = mysqli_prepare($connect, "INSERT INTO users (name, age, telephone, email, password) VALUES (?, ?, ?, ?, ?)");
-    mysqli_stmt_bind_param($stmt, "sisss", $name, $age, $phone, $email, $password);
-
+    $stmt = mysqli_prepare($connect, "SELECT password FROM users  WHERE email=?");
+    mysqli_stmt_bind_param($stmt, "s", $email);
+    
+    // Kontrola ze ci vysiel statement (ziadne errory)
     if (mysqli_stmt_execute($stmt)) {
-        echo "Record inserted successfully.";
+        $result = $stmt->get_result();
+
+        // Existuje pouzivatel?
+        if($result->num_rows == 0){
+            $stmt = mysqli_prepare($connect, "INSERT INTO users (name, age, telephone, email, password) VALUES (?, ?, ?, ?, ?)");
+            mysqli_stmt_bind_param($stmt, "sisss", $name, $age, $phone, $email, $password);
+            if (mysqli_stmt_execute($stmt)) {
+                echo "Account created.";
+            } else {
+                echo "Error: " . mysqli_error($connect);
+            }
+        } else {
+            echo "Email already exists.";
+        }
+
     } else {
         echo "Error: " . mysqli_error($connect);
     }
