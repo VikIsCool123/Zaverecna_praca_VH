8c99018caa19cea1cb4b1e57ad70c09a6492da27
VikIsCool123 <viktorhorvath876@gmail.com>
2026-05-12 14:26:37 +0200

    improoved styles

--- profile.php
+++ profile.php
@@ -10,12 +10,9 @@
     <body class="body-reservation" background="images/book.png"><!--The body of the webpage that has an image as the bacground.-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
         <?php include "parts/header.php"?>
-        <div class="container-profile"><!--Container for the form.-->
-
-        </div>
         <div class="container-section">
-            <div class="container1">
-                <div class="container-index-texts"><!--The banner with the welcome text.-->
+            <div class="container-profile">
+                <div class="container-profile-box"><!--The banner with the welcome text.-->
                     <?php include "profile_info.php"?>
                     <h2>Profile</h2>
                     <p>Username: <?php echo $user["name"] ?></p>
@@ -28,6 +25,9 @@
                             <button class="hero-button1">Log out</button>
                         </a>
                     </div>
+                </div>
+                <div class="container-profile-box-reservations">
+                    <h2>Reservations</h2>
                 </div>
             </div>
         </div>
--- readme.md
+++ readme.md
@@ -3,15 +3,15 @@
 ## Main things
 
 - Config file and sql file
-- Finish reservation logic / sql
-- add error message for reservations
 - Add admin page to see users
 - Add reservations on the right of the profile page
 - Zabranit pripejenie na account cez link
-- Show price per session from programs table
 
 ## Extra things
 
 - Change age to date of birth
 - Change background in profile page
 - Allow changing password in profile
+- add error message for reservations
+- remove reservation
+- Show price per session from programs table
