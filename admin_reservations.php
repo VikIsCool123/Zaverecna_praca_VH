82a325327651162719559636f2621af50f427685
VikIsCool123 <viktorhorvath876@gmail.com>
2026-05-15 19:16:20 +0200

    add reservations admin table

--- admin_reservations.php
+++ admin_reservations.php
@@ -1,37 +1,43 @@
 <!doctype html>
-<html>
+<html lang="en">
     <head><!--The head of the webpage where all the links are stored.-->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>LuiRC</title>
+        <title>LuiRC - Reservation</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
         <link rel="stylesheet" href="Style/style.css"><!--The link/path to the style file with all the styles used to style the webpage.-->
     </head>
-    <body>
-        <?php
-            $connect = mysqli_connect("localhost","root","","relaxation_center");
-            $reservations = mysqli_query($connect,"SELECT * FROM reservations");
-
-            echo '<table class="reservation-container">';
-            echo "<tr>";
-            echo "<th>Name</th>";
-            echo "<th>Age</th>";
-            echo "<th>Email</th>";
-            echo "<th>Phone</th>";
-            echo "<th>Note</th>";
-            echo "<th>Program</th>";
-            echo "</tr>";
-            while ($single_reservation = mysqli_fetch_assoc($reservations)) {
-                echo "<tr>";
-                echo "<td>".$single_reservation["name"]."</td>";
-                echo "<td>".$single_reservation["age"]."</td>";
-                echo "<td>".$single_reservation["email"]."</td>";
-                echo "<td>".$single_reservation["phone"]."</td>";
-                echo "<td>".$single_reservation["note"]."</td>";
-                echo "<td>".$single_reservation["program"]."</td>";
-                echo "</tr>";
-            }
-            echo "</table>";
-        ?>
+    <body class="body-reservation" background="images/book.png"><!--The body of the webpage that has an image as the bacground.-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+        <?php include "parts/header.php"?>
+        <?php if(!isset($_SESSION["user_id"])){
+            header("Location: index.php");
+        }?>
+        <div class="container-section">
+            <div class="container-admin">
+                <div class="container-admin-box"><!--The banner with the welcome text.-->
+                    <?php
+                        include "reservations.php";
+                        $reservations = new Reservations();
+                        $all_reservations = $reservations -> getAllReservations();
+                    ?>
+                    <table class="admin-table">
+                        <tr>
+                            <th>User ID</th>
+                            <th>Program</th>
+                            <th>Date</th>
+                        </tr>
+                        <?php foreach ($all_reservations as $reservation): ?>
+                            <tr>
+                                <td><?php echo $reservation["user_id"] ?></td>
+                                <td><?php echo $reservation["name"] ?></td>
+                                <td><?php echo $reservation["time"] ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <?php include "parts/footer.php"?>
     </body>
 </html>
